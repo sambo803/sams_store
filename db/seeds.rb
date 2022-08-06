a928fe43c186350765432988f009a392f9842900
samuel <sbacon803@gmail.com>
2022-08-05 23:35:33 -0600

    add controller, model, db, and 2 products in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(name: "lamp", price: 29, description: "sheds light", image_url: "https://www.ikea.com/us/en/images/products/lauters-table-lamp-with-led-bulb-ash-white__0879402_pe714879_s5.jpg")
+
+product = Product.new(name: "chair", price: 50, description: "you sit on it", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc9NnExtmUtykibRy7OaQ5nYtIH-0PzSDuPA&usqp=CAU")
+product.save
