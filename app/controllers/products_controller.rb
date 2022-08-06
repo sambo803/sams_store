d1edb1126ca9ea75dd1ea769ad8db407fec98c45
samuel <sbacon803@gmail.com>
2022-08-06 12:13:02 -0600

    add actions and test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,38 @@
 class ProductsController < ApplicationController
+  def index
+    products = Product.all
+    render json: products.as_json
+  end
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image_url: params[:image_url],
+    )
+    product.save
+    render json: product.as_json
+  end
+
+  def show
+    product = Product.find_by(id: params[:id])
+    render json: product.as_json
+  end
+
+  def update
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.description = params[:description] || product.description
+    product.image_url = params[:image_url] || product.image_url
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: { message: "product is removed" }
+  end
 end
